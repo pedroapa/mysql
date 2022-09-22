1afb10b7e1d064253a1da289f70fc3d45c8a440f
pedroapa <111130865+pedroapa@users.noreply.github.com>
2022-09-22 13:31:38 -0300

    Continuei o código
    Tinha mais um pouco do vídeo para ver. Neste eu aprendi a usar o IN, ele  é o jeito otimizado de ver varias opções da mesma característica. Ex ver quais cursos saíram não apenas em um ano. Em vez de ficar fazendo assim:
    where ano = '2015' OR ano = '2016' OR ano = '2019';
    Você usa só isso:
    where ano IN ('2015', '2016', '2019')
    
    E é diferente do BETWEEN, que mostra registros que pertence a uma faixa de valores. Ex: Todos os cursos entre 2015 a 2019. Se tiver cadastrado, vai mostrar 2015, 2016, 2017, 2018 e 2019.

--- script/9-21/select.sql
+++ script/9-21/select.sql
@@ -18,6 +18,13 @@ where ano != '2016'
 order by ano, nome;
 
 select nome, ano from cursos
-where ano between '2014' and '2016'
+where ano between '2014' and '2019'
 order by ano desc, nome;
 
+select nome, descricao, ano from cursos
+where ano in ('2014', '2016', '2019')
+order by ano;
+
+select nome, carga, totaulas from cursos
+where carga > 35 AND totaulas < 30
+order by nome;
