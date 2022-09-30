9e690d95c8ccb4210549378bf630f38898169946
pedroapa <111130865+pedroapa@users.noreply.github.com>
2022-09-30 11:09:26 -0300

    Script ate o momento
    La em baixo to testando de usar um select dentro de outro, mas ainda não aprendi a colocar nomes na coluna, na verdade eu sabia fazer isso, mas por fazer bastante tempo e seguir o curso em vídeo, não vou usar isso.

---
+++ Material/script/9-29/Exercicios/2/qtd-homens-mulhers-nasc-depois-desse-ano.sql
@@ -0,0 +1,18 @@
+/*"Quantos gafanhotos homens e quantas mulheres nasceram após 01/jan/2005?"*/
+
+use cadastro;
+
+describe gafanhotos;
+
+select * from gafanhotos;
+
+select count(*) from gafanhotos
+group by sexo
+having sexo = 'M';
+
+select (select count(*) from gafanhotos group by sexo having sexo = 'M') from gafanhotos;
+
+
+/* "Selecione contabilizando a quantidade de homens da tabela gafanhotos"*/
+select count(*) from gafanhotos
+where sexo = 'M';
